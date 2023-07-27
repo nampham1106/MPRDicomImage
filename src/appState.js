35e8ab743747fdc67add66d2f27a47f2a6324876
Duong Nam <dnam.dvn@gmail.com>
2023-07-27 09:45:35 +0700

    Base code

---
+++ src/appState.js
@@ -0,0 +1,9 @@
+import study0 from 'studies/0.js';
+
+const series = [];
+series[0] = study0;
+
+export default {
+    series,
+    vtkVolumes: []
+}
---
+++ src/lib/tryGetMetadataModuleAsync.js
@@ -0,0 +1,14 @@
+import * as cornerstone from 'cornerstone-core';
+import getUrlForImageId from './getUrlForImageId.js';
+
+export default async function(metadataModule, imageId){
+    const imageUrl = getUrlForImageId(imageId);
+    let imageMetadata = cornerstone.metaData.get(metadataModule, imageUrl)
+
+    if(!imageMetadata){
+        await cornerstone.loadAndCacheImage(imageUrl);
+        imageMetadata = cornerstone.metaData.get(metadataModule, imageUrl);
+    }
+    
+    return imageMetadata;
+}
